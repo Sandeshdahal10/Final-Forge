4a8533b5e38fc20ae96bbec6788d219375c57b4c
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-03-18 17:18:52 +0545

    import error.js file to app.js.

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
+import errorMiddleware from "./middlewares/error.js";
 
 
 config();
@@ -18,6 +19,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(errorMiddleware);
 
 export default app;
 
