5718e36f82188d82c159495dbe6ffec9670b2d1d
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-03-19 19:55:35 +0545

    Created register User Functiion in authController.js

---
+++ server/controllers/authController.js
@@ -0,0 +1,30 @@
+//Register User
+
+import { asyncHandler } from "../middlewares/asyncHandler.js";
+import ErrorHandler from "../middlewares/error.js";
+import User from "../models/user.js";
+import { generateToken } from "../utils/generateToken.js";
+
+export const registerUser = asyncHandler(async (req,res,next) => {
+    const {name,email,password,role} = req.body;
+    if(!name || !email || !password || !role){
+        return next(new ErrorHandler("Please provide all required fields"));
+    }
+    let user = await User.findOne({email});
+    if(user){
+        return next(new ErrorHandler("User already exists with this email",400));
+    }
+    user = new User ({
+        name,email,password,role
+    });
+    await user.save();
+    generateToken(user,201,"User registered successfully",res);
+});
+
+export const login = asyncHandler(async (req,res,next) => {
+    
+});
+export const getUser = asyncHandler(async (req,res,next) => {});
+export const logout = asyncHandler(async (req,res,next) => {});
+export const forgotPassword = asyncHandler(async (req,res,next) => {});
+export const resetPassword = asyncHandler(async (req,res,next) => {});
