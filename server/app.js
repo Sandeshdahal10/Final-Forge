b62eb0cc8ce4e80dbb757a1c03544b5774e4cb32
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-03-19 20:21:25 +0545

    Fixed the authRouter extra parenthesis error.

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(("/api/v1/auth", authRouter));
+app.use("/api/v1/auth", authRouter);
 app.use(errorMiddleware);
 
 export default app;
