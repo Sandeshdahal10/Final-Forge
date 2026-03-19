b2d8c88c15ca5bc711648742fa63ce5aaa5167e2
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-03-19 19:53:47 +0545

    Created User Model Schema and added JWT token generation method.

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,81 @@
-import bcrypt from 'bcrypt';
-import mongoose from 'mongoose';
-import jwt from 'jsonwebtoken';
-import crypto from 'crypto';
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+import { type } from "os";
 
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your name"],
+      trim: true,
+      maxLength: [50, "Your name cannot exceed 50 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter your email"],
+      unique: true,
+      lowercase: true,
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        "Please enter a valid email",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter your password"],
+      minLength: [8, "Your password must be at least 8 characters long"],
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: ["Student", "Teacher", "Admin"],
+      default: "Student",
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+
+    department: {
+      type: String,
+      trim: true,
+      default: null,
+    },
+    expertise: {
+      type: [String],
+      default: [],
+    },
+    maxStudents: {
+      type: Number,
+      default: 10,
+      min: [1, "Min students must be at least 1"],
+    },
+    assignedStudents: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    supervisor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+    project: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project",
+      default: null,
+    },
+  },
+  { timestamps: true },
+);
+
+userSchema.methods.generateToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
+export const User = mongoose.model("User", userSchema);
+
+export default User;
