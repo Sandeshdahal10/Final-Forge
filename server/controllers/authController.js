4499fb0baa024d2c80a821cbc9d701b83bcdb2f7
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-03-20 17:56:39 +0545

    Created Login function .

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,26 +5,41 @@ import ErrorHandler from "../middlewares/error.js";
 import User from "../models/user.js";
 import { generateToken } from "../utils/generateToken.js";
 
-export const registerUser = asyncHandler(async (req,res,next) => {
-    const {name,email,password,role} = req.body;
-    if(!name || !email || !password || !role){
-        return next(new ErrorHandler("Please provide all required fields"));
-    }
-    let user = await User.findOne({email});
-    if(user){
-        return next(new ErrorHandler("User already exists with this email",400));
-    }
-    user = new User ({
-        name,email,password,role
-    });
-    await user.save();
-    generateToken(user,201,"User registered successfully",res);
+export const registerUser = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+  if (!name || !email || !password || !role) {
+    return next(new ErrorHandler("Please provide all required fields"));
+  }
+  let user = await User.findOne({ email });
+  if (user) {
+    return next(new ErrorHandler("User already exists with this email", 400));
+  }
+  user = new User({
+    name,
+    email,
+    password,
+    role,
+  });
+  await user.save();
+  generateToken(user, 201, "User registered successfully", res);
 });
 
-export const login = asyncHandler(async (req,res,next) => {
-    
+export const login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(new ErrorHandler("Please provide email and password", 400));
+  }
+  const user = await User.findOne({ email, role }).select("+password");
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password or role", 401));
+  }
+  const isPasswordMatch = await user.comparePassword(password);
+  if (!isPasswordMatch) {
+    return next(new ErrorHandler("Invalid password", 401));
+  }
+  generateToken(user, 200, "User logged in successfully", res);
 });
-export const getUser = asyncHandler(async (req,res,next) => {});
-export const logout = asyncHandler(async (req,res,next) => {});
-export const forgotPassword = asyncHandler(async (req,res,next) => {});
-export const resetPassword = asyncHandler(async (req,res,next) => {});
+export const getUser = asyncHandler(async (req, res, next) => {});
+export const logout = asyncHandler(async (req, res, next) => {});
+export const forgotPassword = asyncHandler(async (req, res, next) => {});
+export const resetPassword = asyncHandler(async (req, res, next) => {});
